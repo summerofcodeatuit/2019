2c4524695d76feef64d76e4ecae29654671ae82f
whoami-shubham <shubhamkumarjha0013@gmail.com>
2019-03-29 15:54:48 +0530

    Updates

---
+++ precache-manifest.e5856b35b4dcbe3af9ecc5fad04a2443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18758d7980354d39504d4ee4117ef5d1",
+    "url": "/2019/static/media/logo.18758d79.png"
+  },
+  {
+    "revision": "d576c0a33d199b74dd90081301e1eae5",
+    "url": "/2019/static/media/home3.d576c0a3.jpg"
+  },
+  {
+    "revision": "5b659db50188d87137d1",
+    "url": "/2019/static/js/runtime~main.8f2f7504.js"
+  },
+  {
+    "revision": "467e60d270e82731bc11",
+    "url": "/2019/static/js/main.b5dafbe2.chunk.js"
+  },
+  {
+    "revision": "3782c121f9c06343cce9",
+    "url": "/2019/static/js/2.754db3e6.chunk.js"
+  },
+  {
+    "revision": "465909738be26b59caed7ff1b38e45d2",
+    "url": "/2019/index.html"
+  }
+];
